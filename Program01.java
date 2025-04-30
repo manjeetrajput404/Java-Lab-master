2408418415df292b52c79ca1a1a79324e69c6892
Manjeet <manjeetsinghji999114@gmail.com>
2025-04-30 10:40:40 +0530

    Rename Program1.java to Program01.java

---
+++ Program01.java
@@ -0,0 +1,1 @@
+// It contains only the therory portion i.e how to setup java and jdk on different OS.
--- Program1.java
+++
@@ -1,1 +0,0 @@
-// It contains only the therory portion i.e how to setup java and jdk on different OS.
